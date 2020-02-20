3aeafac8b045fae7215f849334120440a3823242
Jen-Hobbs <jen.g.hobbs@gmail.com>
2020-02-19 18:40:43 -0800

    add animations

--- JS/Routing.js
+++ JS/Routing.js
@@ -1,4 +1,4 @@
-var app = angular.module("templater", ["ngRoute"]);
+var app = angular.module("templater", ["ngRoute", 'ngAnimate']);
 app.config(function($routeProvider){
     $routeProvider
     .when("/", {
